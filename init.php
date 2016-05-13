f4b8c4b035aa1560b7ad0a8e612dc7826667ec06
Asmodai <asmodai@asmodai-ubuntu.lan>
2016-05-13 16:30:04 +0300

    udpate

--- init.php
+++ init.php
@@ -10,6 +10,9 @@ require_once __DIR__ . '/src/config.php';
 if (DEBUG_MODE) {
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
+}else{
+    error_reporting(E_CORE_ERROR);
+    ini_set('display_errors', 'off');
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
